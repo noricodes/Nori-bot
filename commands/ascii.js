954ccf4a0a78bbe08310f651c11ad2d104463585
naorinn <33076168+naorinn@users.noreply.github.com>
2019-05-31 14:04:10 -0400

    Create ascii.js

---
+++ commands/ascii.js
@@ -0,0 +1,20 @@
+const ascii = require('ascii-art');
+
+exports.run = (client, message, args, ops) => {
+
+  ascii.font(args.join(' '), 'Doom', function(rendered){
+    //ascii.font(args.join(' '), 'Doom', function(rendered){
+
+    rendered = rendered.trimRight();
+
+    if(rendered.length > 2000 ) return message.channel.send('Sorry, that message is too long');
+
+    message.channel.send(rendered, {
+      code: 'md'
+    })
+
+  });
+
+   
+
+}
